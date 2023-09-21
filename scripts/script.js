7f7c8f17c83c68e01cd10403ce90bc35127414a0
Brock Pavelko <bpavelko624@gmail.com>
2023-09-21 17:23:46 -0400

    home page updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", function() {
     var preloader = document.querySelector(".preloader");
     preloader.classList.add("hide");
-  });
+});
+
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector(".navbar");
+    if (window.scrollY >= document.querySelector(".hero-image").offsetHeight) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+})
 
 function myFunction() {
     var x = document.getElementById("myNavbar");
@@ -9,5 +18,23 @@ function myFunction() {
         x.className += " responsive";
     } else {
         x.className = "navbar";
+    }
+}
+
+function scrollToGamesSection() {
+    const gamesSection = document.getElementById("games");
+
+    // Scroll smoothly to the games section
+    if (gamesSection) {
+        gamesSection.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+function scrollToCookies() {
+    const cookieSection = document.getElementById("cookies");
+
+    // Scroll smoothly to the games section
+    if (cookieSection) {
+        cookieSection.scrollIntoView({ behavior: "smooth" });
     }
 }
