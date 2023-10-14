4f0d1bcc47713ebe5c2b467a264cedb94d88218e
Brock Pavelko <bpavelko624@gmail.com>
2023-10-13 23:19:36 -0400

    little update for homepage, need to add more

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,16 @@ function myFunction() {
         x.className = "navbar";
     }
 }
+
+window.addEventListener("scroll", function() {
+    const rows = document.querySelectorAll(".row");
+
+    rows.forEach(row => {
+        const left = document.querySelector(".left");
+        const right = document.querySelector(".right");
+        const scrollPosition = window.scrollY;
+
+        left.style.transform = `translateY(${scrollPosition * 0.1}px)`;
+        right.style.transform = `translateY(${scrollPosition * 0.6}px)`;
+    });
+});
